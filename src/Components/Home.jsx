5b0596bd5cc1cc87957970c7e5df92e04f41f587
VinuBhau <roddvinayak98@gmail.com>
2025-04-06 10:03:39 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -154,22 +154,34 @@ const Home = () => {
       <img src={SynergyIcon}  className="ml-0 rounded-full lg:ml-2 lg:mt-2 w-16 h-16 lg:w-20 lg:h-20 " />
 
        {/* Desktop Navigation */}
-      <div className=" flex flex-row gap-6 lg:gap-10 justify-center items-center mr-5">
-      <Link to="/courses" className="text-white text-lg font-semibold" >
-            <div className='lg:w-[200px] lg:h-[50px] w-[100px] h-[45px] flex flex-row gap-1.5 lg:gap-3 items-center justify-center p-2 rounded-3xl bg-[#d72c2c]'>
-            <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" className="bi bi-ui-checks-grid w-[20px] h-[20px]" viewBox="0 0 16 16">
-              <path d="M2 10h3a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1m9-9h3a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-3a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1m0 9a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1zm0-10a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h3a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM2 9a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h3a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2zm7 2a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-3a2 2 0 0 1-2-2zM0 2a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm5.354.854a.5.5 0 1 0-.708-.708L3 3.793l-.646-.647a.5.5 0 1 0-.708.708l1 1a.5.5 0 0 0 .708 0z"/>
-            </svg>
-                <h1 className='text-white text-center font-medium text-xs lg:text-xl'>Dashboard</h1>
-            </div>
-      </Link>
+       {/* Desktop Navigation */}
+      {authUser !== null ? 
+        <div className=" flex flex-row gap-6 lg:gap-10 justify-center items-center mr-5">
 
-      <div className='flex flex-row gap-2'>
-            
+          <Link to="/courses" className="text-white text-lg font-semibold" >
+                <div className='lg:w-[200px] lg:h-[50px] w-[100px] h-[45px] flex flex-row gap-1.5 lg:gap-3 items-center justify-center p-2 rounded-3xl bg-[#d72c2c]'>
+                <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" className="bi bi-ui-checks-grid w-[20px] h-[20px]" viewBox="0 0 16 16">
+                  <path d="M2 10h3a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1m9-9h3a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-3a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1m0 9a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1zm0-10a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h3a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM2 9a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h3a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2zm7 2a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-3a2 2 0 0 1-2-2zM0 2a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm5.354.854a.5.5 0 1 0-.708-.708L3 3.793l-.646-.647a.5.5 0 1 0-.708.708l1 1a.5.5 0 0 0 .708 0z"/>
+                </svg>
+                    <h1 className='text-white text-center font-medium text-xs lg:text-xl'>Dashboard</h1>
+                </div>
+          </Link>
+
+          <div className='flex flex-row gap-2'>
+                      
             <LogOut className="cursor-pointer "  />
-      </div>
+          </div>
+        </div>
 
-      </div>
+        :
+        <Link to="/login">
+        <div className='lg:w-[150px] lg:h-[50px] lg:mr-5 w-[80px] h-[45px] flex flex-row gap-1.5 lg:gap-3 items-center justify-center p-2 rounded-lg bg-[#d72c2c]'>
+                    <h1 className='text-white text-center font-medium text-base lg:text-lg'>Login</h1>
+        </div>
+        </Link>}
+
+      
+
 
     </div>
 
